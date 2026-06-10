5249699b7e6a2342973db4d58e5811608104a484
atuolan <a23971123@gmail.com>
2026-06-10 14:40:29 +0800

    add: 刪除垃圾

--- test_regex.js
+++
@@ -1,5 +0,0 @@
-const attrs = 'type="rename" actor="雪拉比" value="✨ 星光與森林的小窩 ✨"';
-const name = 'value';
-const regex = new RegExp(`${name}=["']([^"']+)["']`, "i");
-const match = attrs.match(regex);
-console.log('Match:', match ? match[1] : 'null');
